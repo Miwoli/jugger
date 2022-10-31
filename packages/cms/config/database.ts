9913f5ce5ccbb6456bec4853e65e92921a2da111
Michał Woliński <1531829-Miwoli@users.noreply.gitlab.com>
2022-10-31 13:00:43 +0100

    feat: prepare Strapi for develop deployment

--- packages/cms/config/database.ts
+++ packages/cms/config/database.ts
@@ -1,13 +1,13 @@
 export default ({ env }) => ({
   connection: {
-    client: "mysql",
+    client: 'mysql',
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "jugger"),
-      user: env("DATABASE_USERNAME", "user"),
-      password: env("DATABASE_PASSWORD", "pass"),
-      ssl: env.bool("DATABASE_SSL", false),
-    },
-  },
-});
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DB_NAME', 'jugger'),
+      user: env('DB_USER', 'user'),
+      password: env('DB_PASS', 'pass'),
+      ssl: env.bool('DATABASE_SSL', false)
+    }
+  }
+})
